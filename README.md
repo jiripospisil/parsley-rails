99a3be8ca7ac51a648ca1756af3e1316b8ef33fe
Robert Wiegmann <robert.wiegmann@regeneron.com>
2014-04-02 12:12:32 -0400

    Updated for 2.0.0-rc5; shell script updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Parsley.js for Rails Asset Pipeline
+# Parsley.js 2.0.0 for Rails Asset Pipeline
 
 [Parsley.js](https://github.com/guillaumepotier/Parsley.js) is an awesome form validation library ([documentation](http://parsleyjs.org/doc/index.html)). 
 This gem makes it easy to use it with Rails Asset Pipeline.
@@ -20,19 +20,19 @@ Add the following to your `app/assets/javascripts/application.js`:
     //= require jquery
     //= require parsley
 
-To use the `extend` library, add this right after:
+To use the `remote` library, add this right after:
 
-    //= require parsley.extend
+    //= require parsley.remote
 
 Finally, if you want to use a different i18n:
 
-    //= require parsley.i18n.fr
+    //= require parsley/i18n/fr
 
 See the complete [Parsley.js documentation](http://parsleyjs.org/doc/index.html).
 
 ## Versions
 
-The gem follows Parsley.js's versions. If the gem version is `1.1.10.0`, then `1.1.10` is a version of Parsley.js and `0`
+The gem follows Parsley.js's versions. If the gem version is `2.0.0.0`, then `2.0.0` is a version of Parsley.js and `0`
 is a patch level of the gem itself.
 
 ## Contributing
--- vendor/assets/javascripts/parsley.i18n.en.js
+++ vendor/assets/javascripts/parsley.i18n.en.js
@@ -1,48 +1,33 @@
-/**
-* /!\ This file is just an example template to create/update your own language file /!\
-*/
-
+// ParsleyConfig definition if not already set
 window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
 
-(function ($) {
-  window.ParsleyConfig = $.extend( true, {}, window.ParsleyConfig, {
-    messages: {
-      // parsley //////////////////////////////////////
-        defaultMessage: "This value seems to be invalid."
-        , type: {
-            email:      "This value should be a valid email."
-          , url:        "This value should be a valid url."
-          , urlstrict:  "This value should be a valid url."
-          , number:     "This value should be a valid number."
-          , digits:     "This value should be digits."
-          , dateIso:    "This value should be a valid date (YYYY-MM-DD)."
-          , alphanum:   "This value should be alphanumeric."
-          , phone:      "This value should be a valid phone number."
-        }
-      , notnull:        "This value should not be null."
-      , notblank:       "This value should not be blank."
-      , required:       "This value is required."
-      , regexp:         "This value seems to be invalid."
-      , min:            "This value should be greater than or equal to %s."
-      , max:            "This value should be lower than or equal to %s."
-      , range:          "This value should be between %s and %s."
-      , minlength:      "This value is too short. It should have %s characters or more."
-      , maxlength:      "This value is too long. It should have %s characters or less."
-      , rangelength:    "This value length is invalid. It should be between %s and %s characters long."
-      , mincheck:       "You must select at least %s choices."
-      , maxcheck:       "You must select %s choices or less."
-      , rangecheck:     "You must select between %s and %s choices."
-      , equalto:        "This value should be the same."
+// Define then the messages
+window.ParsleyConfig.i18n.en = $.extend(window.ParsleyConfig.i18n.en || {}, {
+  defaultMessage: "This value seems to be invalid.",
+  type: {
+    email:        "This value should be a valid email.",
+    url:          "This value should be a valid url.",
+    number:       "This value should be a valid number.",
+    integer:      "This value should be a valid integer.",
+    digits:       "This value should be digits.",
+    alphanum:     "This value should be alphanumeric."
+  },
+  notblank:       "This value should not be blank.",
+  required:       "This value is required.",
+  pattern:        "This value seems to be invalid.",
+  min:            "This value should be greater than or equal to %s.",
+  max:            "This value should be lower than or equal to %s.",
+  range:          "This value should be between %s and %s.",
+  minlength:      "This value is too short. It should have %s characters or more.",
+  maxlength:      "This value is too long. It should have %s characters or less.",
+  length:         "This value length is invalid. It should be between %s and %s characters long.",
+  mincheck:       "You must select at least %s choices.",
+  maxcheck:       "You must select %s choices or less.",
+  check:          "You must select between %s and %s choices.",
+  equalto:        "This value should be the same."
+});
 
-      // parsley.extend ///////////////////////////////
-      , minwords:       "This value should have %s words at least."
-      , maxwords:       "This value should have %s words maximum."
-      , rangewords:     "This value should have between %s and %s words."
-      , greaterthan:    "This value should be greater than %s."
-      , lessthan:       "This value should be less than %s."
-      , beforedate:     "This date should be before %s."
-      , afterdate:      "This date should be after %s."
-      , americandate:	"This value should be a valid date (MM/DD/YYYY)."
-    }
-  });
-}(window.jQuery || window.Zepto));
+// If file is loaded after Parsley main file, auto-load locale
+if ('undefined' !== typeof window.ParsleyValidator)
+  window.ParsleyValidator.addCatalog('en', window.ParsleyConfig.i18n.en, true);
