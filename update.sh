7cb7e1f08546594391ea507bc9351d6e873d4b53
Jiri Pospisil <mekishizufu@gmail.com>
2014-06-14 00:02:30 +0200

    Include the default CSS

--- update.sh
+++ update.sh
@@ -13,19 +13,21 @@ FILE_NAME="$EXTRACT_DIRECTORY.tar.gz"
 RELEASE_URL="https://github.com/guillaumepotier/Parsley.js/archive/$TAG.tar.gz"
 RELEASE_PATH="$EXTRACT_DIRECTORY/Parsley.js-$TAG"
 
-VENDOR_PATH="vendor/assets/javascripts"
+VENDOR_JS_PATH="vendor/assets/javascripts"
+VENDOR_CSS_PATH="vendor/assets/stylesheets"
 
 wget -O $FILE_NAME $RELEASE_URL 
 mkdir $EXTRACT_DIRECTORY
 tar xfz $FILE_NAME -C $EXTRACT_DIRECTORY
 
-cp "$RELEASE_PATH/dist/parsley.js" $VENDOR_PATH
-cp "$RELEASE_PATH/dist/parsley.remote.js" $VENDOR_PATH
+cp "$RELEASE_PATH/dist/parsley.js" $VENDOR_JS_PATH
+cp "$RELEASE_PATH/dist/parsley.remote.js" $VENDOR_JS_PATH
+cp "$RELEASE_PATH/src/parsley.css" $VENDOR_CSS_PATH
 
 for file in "$RELEASE_PATH/src/i18n/"*; do
   base_name=$(basename "$file")
   final_name="parsley.i18n.$(echo $base_name)"
-  cp $file "$VENDOR_PATH/$final_name"
+  cp $file "$VENDOR_JS_PATH/$final_name"
 done
 
 
