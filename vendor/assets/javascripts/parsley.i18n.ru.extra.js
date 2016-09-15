569470b879891f25d80252294620faa403a3f732
dmitchenko <mitchenkod@gmail.com>
2016-09-15 09:16:32 +0700

    some fixes

--- vendor/assets/javascripts/parsley.i18n.ru.extra.js
+++ vendor/assets/javascripts/parsley.i18n.ru.extra.js
@@ -1,7 +1,4 @@
-// Validation errors messages for Parsley
-import Parsley from '../parsley';
-
-Parsley.addMessages('ru', {
+window.Parsley.addMessages('ru', {
   dateiso:  "Это значение должно быть корректной датой (ГГГГ-ММ-ДД).",
   minwords: "Это значение должно содержать не менее %s слов.",
   maxwords: "Это значение должно содержать не более %s слов.",
