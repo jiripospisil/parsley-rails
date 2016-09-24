50dd52fd892d79cf61138bff13342eac507d8bee
Jiri Pospisil <mekishizufu@gmail.com>
2016-09-24 16:17:52 +0200

    Keep the non-min version updated as well

--- update.sh
+++ update.sh
@@ -16,10 +16,11 @@ RELEASE_PATH="$EXTRACT_DIRECTORY/Parsley.js-$TAG"
 VENDOR_JS_PATH="vendor/assets/javascripts"
 VENDOR_CSS_PATH="vendor/assets/stylesheets"
 
-wget -O $FILE_NAME $RELEASE_URL 
+wget -O $FILE_NAME $RELEASE_URL
 mkdir $EXTRACT_DIRECTORY
 tar xfz $FILE_NAME -C $EXTRACT_DIRECTORY
 
+cp "$RELEASE_PATH/dist/parsley.js" $VENDOR_JS_PATH
 cp "$RELEASE_PATH/dist/parsley.min.js" $VENDOR_JS_PATH
 cp "$RELEASE_PATH/src/parsley.css" $VENDOR_CSS_PATH
 
@@ -28,6 +29,5 @@ for file in "$RELEASE_PATH/dist/i18n/"*; do
   final_name="parsley.i18n.$(echo $base_name)"
   cp $file "$VENDOR_JS_PATH/$final_name"
 done
-
 
 rm -rf $FILE_NAME $EXTRACT_DIRECTORY
