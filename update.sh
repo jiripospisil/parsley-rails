18421ad5d0235ea70a53bb2155b1da4f4a5f85e3
Jiri Pospisil <mekishizufu@gmail.com>
2017-03-20 22:27:28 +0100

    Copy extras

--- update.sh
+++ update.sh
@@ -27,6 +27,15 @@ cp "$RELEASE_PATH/dist/parsley.min.js" $VENDOR_JS_PATH
 cp "$RELEASE_PATH/dist/parsley.min.js.map" $VENDOR_JS_PATH
 cp "$RELEASE_PATH/src/parsley.css" $VENDOR_CSS_PATH
 
+# Copy extras
+cp "$RELEASE_PATH/src/extra/plugin/bind.js" "$VENDOR_JS_PATH/parsley.plugin.bind.js"
+cp "$RELEASE_PATH/src/extra/validator/comparison.js" "$VENDOR_JS_PATH/parsley.validator.comparison.js"
+cp "$RELEASE_PATH/src/extra/validator/dateiso.js" "$VENDOR_JS_PATH/parsley.validator.dateiso.js"
+cp "$RELEASE_PATH/src/extra/validator/luhn.js" "$VENDOR_JS_PATH/parsley.validator.luhn.js"
+cp "$RELEASE_PATH/src/extra/validator/notequalto.js" "$VENDOR_JS_PATH/parsley.validator.notequalto.js"
+cp "$RELEASE_PATH/src/extra/validator/words.js" "$VENDOR_JS_PATH/parsley.validator.words.js"
+
+# Copy the i18n files
 for file in "$RELEASE_PATH/dist/i18n/"*; do
   base_name=$(basename "$file")
   final_name="parsley.i18n.$(echo $base_name)"
